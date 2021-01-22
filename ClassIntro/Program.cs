49d1effc70b574f828bd5ca806ff9400623818e4
Ebru Özdil <ebruozdil99@gmail.com>
2021-01-23 02:54:56 +0300

    Add project files.

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adi = "Ebru";
+            int yas = 36;
+
+            Kurs kurs1 = new Kurs();
+            kurs1.KursAdi = "C#";
+            kurs1.Egitmen = "Engin Demiroğ";
+            kurs1.IzlenmeOranı = 68;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "Java";
+            kurs2.Egitmen = "Kerem Varış";
+            kurs2.IzlenmeOranı = 64;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.KursAdi = "Python";
+            kurs3.Egitmen = "Berkan Bilgin";
+            kurs3.IzlenmeOranı = 80;
+
+            Kurs kurs4 = new Kurs();
+            kurs4.KursAdi = "C++";
+            kurs4.Egitmen = "Murat Kurtboğan";
+            kurs4.IzlenmeOranı = 100;
+
+            //Command line-yorum satırı
+            //Console.WriteLine(kurs1.KursAdi + " ; "+ kurs1.Egitmen);
+
+            //string[] kurslar = new string[] {}   Böyle yazınca sadece içinde string tutuyorduk kurs yazınca kurs ların objelerini tutabiliyoruz.
+            Kurs[] kurslar = new Kurs[] {kurs1, kurs2 ,kurs3 ,kurs4};
+
+            foreach (var  kurs in  kurslar)
+            {
+                Console.WriteLine(kurs.KursAdi+ " ; " + kurs.Egitmen);
+            }
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+    // class kursla yeni bi değişken türü gibi bişey oluşturmuş oluyoruz - tipimiz kurs oluyo
+    class Kurs
+    {
+        //prop (özellik anlamında) yazıp tab tab yapıyoruz
+        public string KursAdi { get; set; }
+        public string Egitmen { get; set; }
+        public int IzlenmeOranı { get; set; }
+    }
+}
---
+++ KampIntroo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace KampIntroo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güvenliği
+            //Do not repeat yourself - Kendini tekrarlama
+            //değer tutucu, alias -kategoriEtiketi
+            string kategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 32000;
+            double faizOranı = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("değişmedi butonu");
+            }
+            if (sistemeGirisYapmisMi ==true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+          
+        }
+    }
+}
+
